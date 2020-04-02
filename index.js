e52a82826004027b17decfce14ddd6bc7ad16ef3
Mohammed Adinan <adinanaries@outlook.com>
2020-04-02 12:18:02 -0400

    changing names for obvious reasons

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const CMS_Prompt = require("./cms-inquirer");
 //const queries = new Queries();
 const app = new CMS_Prompt();
 console.log(
-  "\n\n========================\nNaomi's EMS\n========================"
+  "\n\n========================\nEmployee Management System\n========================"
 );
 app.runPrompt();
 
